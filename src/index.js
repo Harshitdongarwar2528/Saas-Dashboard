94462e289b6054332950e0a58a31b19166c52461
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-06-05 12:36:06 +0530

    added the context provider to add things form it such as handle click etc wrapped the content with context provider in index.js file, made the full navbar and made it response added active setActiveMenu and imported links dummy data made the context provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { ContextProvider } from "./contexts/ContextProvider";
 import App from "./App";
 import { registerLicense } from "@syncfusion/ej2-base";
 registerLicense(process.env.REACT_APP_SYNCFUSION_LICENSE);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ContextProvider>
+    <App />,
+  </ContextProvider>,
+
+  document.getElementById("root"),
+);
